9c26f025d471f50f28d6af8ebe272c8fa4a55e60
annisasilvia04 <122153114+annisasilvia04@users.noreply.github.com>
2023-11-13 11:43:50 +0700

    Add files via upload

---
+++ AkademikApp/AkademikApp/FrmMahasiswa.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace AkademikApp
+{
+    partial class FrmMahasiswa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvwMahasiswa = new System.Windows.Forms.ListView();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnPerbaiki = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnSelesai = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvwMahasiswa
+            // 
+            this.lvwMahasiswa.HideSelection = false;
+            this.lvwMahasiswa.Location = new System.Drawing.Point(24, 23);
+            this.lvwMahasiswa.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.lvwMahasiswa.Name = "lvwMahasiswa";
+            this.lvwMahasiswa.Size = new System.Drawing.Size(1048, 564);
+            this.lvwMahasiswa.TabIndex = 0;
+            this.lvwMahasiswa.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(24, 602);
+            this.btnTambah.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(150, 44);
+            this.btnTambah.TabIndex = 1;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnPerbaiki
+            // 
+            this.btnPerbaiki.Location = new System.Drawing.Point(186, 602);
+            this.btnPerbaiki.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnPerbaiki.Name = "btnPerbaiki";
+            this.btnPerbaiki.Size = new System.Drawing.Size(150, 44);
+            this.btnPerbaiki.TabIndex = 2;
+            this.btnPerbaiki.Text = "Perbaiki";
+            this.btnPerbaiki.UseVisualStyleBackColor = true;
+            this.btnPerbaiki.Click += new System.EventHandler(this.btnPerbaiki_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(348, 602);
+            this.btnHapus.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(150, 44);
+            this.btnHapus.TabIndex = 3;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnSelesai
+            // 
+            this.btnSelesai.Location = new System.Drawing.Point(926, 602);
+            this.btnSelesai.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSelesai.Name = "btnSelesai";
+            this.btnSelesai.Size = new System.Drawing.Size(150, 44);
+            this.btnSelesai.TabIndex = 4;
+            this.btnSelesai.Text = "Selesai";
+            this.btnSelesai.UseVisualStyleBackColor = true;
+            this.btnSelesai.Click += new System.EventHandler(this.btnSelesai_Click);
+            // 
+            // FrmMahasiswa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 663);
+            this.Controls.Add(this.btnSelesai);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnPerbaiki);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.lvwMahasiswa);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.Name = "FrmMahasiswa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Mahasiswa";
+            this.Load += new System.EventHandler(this.FrmMahasiswa_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvwMahasiswa;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnPerbaiki;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnSelesai;
+    }
+}
+
